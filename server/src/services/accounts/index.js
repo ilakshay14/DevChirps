46818d347a085f3883bde32fb600e67d08f0ee9c
lakshay <malhotra.lakshay93@gmail.com>
2022-01-15 19:45:47 +0530

    initial commit

---
+++ server/src/services/accounts/index.js
@@ -0,0 +1,15 @@
+import { ApolloServer } from 'apollo-server'
+import { buildFederatedSchema } from '@apollo/federation';
+import resolvers from './resolvers';
+import typeDefs from './typeDefs';
+
+(async () => {
+    const port = process.env.ACCOUNTS_SERVICE_PORT;
+
+    const server = new ApolloServer({
+        schema: buildFederatedSchema([{ typeDefs, resolvers }]),
+    });
+
+    const { url } = await server.listen({ port });
+    console.log(`Accounts service ready at ${url}`);
+})()
